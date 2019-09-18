75857a07c33c7bead0cdaac819378f9c19e39495
Sont3k <markus.residence@gmail.com>
2019-09-18 21:52:53 +0300

    Added couple of dataFunctions for users and locations

--- functions/dataFunctions.js
+++ functions/dataFunctions.js
@@ -1,3 +1,5 @@
+// User
+
 export const fetchUsers = async () => {
   const data = await fetch('http://167.71.4.155/user/lists')
     .then(response => response.json())
@@ -7,6 +9,51 @@ export const fetchUsers = async () => {
   console.log(data);
   return data;
 };
+
+export const fetchUser = async (userID) => {
+  const data = await fetch('http://167.71.4.155/user/' + userID)
+    .then(response => response.json())
+    .then(data => data)
+    .catch(error => console.error(error));
+
+  console.log(data);
+  return data;
+};
+
+export const updateUser = (userID, user) => {
+  fetch('http://167.71.4.155/user/' + userID, {
+    method: 'POST',
+    headers: new Headers(),
+    body: JSON.stringify(user),
+  })
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error(error));
+};
+
+export const createUser = (lastUserID, user) => {
+  fetch('http://167.71.4.155/user/' + (lastUserID + 1), {
+    method: 'POST',
+    headers: new Headers(),
+    body: JSON.stringify(user),
+  })
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error(error));
+};
+
+export const deleteUser = (userID) => {
+  fetch('http://167.71.4.155/user/' + userID, {
+    method: 'POST',
+    headers: new Headers(),
+    body: JSON.stringify({}),
+  })
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error(error));
+};
+
+// Location
 
 export const fetchLocations = async () => {
   const data = await fetch('http://167.71.4.155/location/lists')
@@ -18,13 +65,48 @@ export const fetchLocations = async () => {
   return data;
 };
 
-// export const postData = () => {
-//   fetch('http://167.71.4.155/', {
-//     method: 'POST',
-//     headers: new Headers(),
-//     body: JSON.stringify({title: 'Great'}),
-//   })
-//     .then(response => response.json())
-//     .then(data => console.log(data))
-//     .catch(error => console.error(error));
-// };
+export const fetchLocation = async (locationID) => {
+  const data = await fetch('http://167.71.4.155/location/' + locationID)
+    .then(response => response.json())
+    .then(data => data)
+    .catch(error => console.error(error));
+
+  console.log(data);
+  return data;
+};
+
+export const updateLocation = (locationID, location) => {
+  fetch('http://167.71.4.155/location/' + locationID, {
+    method: 'POST',
+    headers: new Headers(),
+    body: JSON.stringify(location),
+  })
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error(error));
+};
+
+export const createLocation = (lastLocationID, location) => {
+  fetch('http://167.71.4.155/location/' + (lastLocationID + 1), {
+    method: 'POST',
+    headers: new Headers(),
+    body: JSON.stringify(location),
+  })
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error(error));
+};
+
+export const deleteLocation = (locationID) => {
+  fetch('http://167.71.4.155/location/' + locationID, {
+    method: 'POST',
+    headers: new Headers(),
+    body: JSON.stringify({}),
+  })
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error(error));
+};
+
+
+
