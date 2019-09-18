2f7570bc5e38ab0286eae79579e9a97fd74688a3
Sont3k <markus.residence@gmail.com>
2019-09-18 14:15:53 +0300

    Added address picker for Users

--- components/Users.js
+++ components/Users.js
@@ -1,32 +1,58 @@
 import React, {useEffect, useState} from 'react';
-import {View, StyleSheet, Image, Text, TouchableOpacity} from 'react-native';
+import {
+  View,
+  StyleSheet,
+  Image,
+  Text,
+  TouchableOpacity,
+  ScrollView,
+  Picker,
+} from 'react-native';
 import {TopBar} from './TopBar';
 import {fetchData, postData} from '../functions/dataFunctions';
-import {ScrollView} from 'react-native-gesture-handler';
 
-const renderUsers = (users, {navigation}) => {
-  console.log(users);
+const renderUsers = (users, {navigation}, address) => {
+  if (users && users.data && address) {
+    return users.data.map(user => {
+      if (user.address == address) {
+        return (
+          <TouchableOpacity
+            key={user.id}
+            style={styles.userCard}
+            onPress={() => navigation.navigate('Location')}>
+            <Image style={styles.userAvatar} source={{uri: user.avatar}} />
+
+            <View style={styles.infoBlock}>
+              <View style={styles.userName}>
+                <Text>{user.name}</Text>
+              </View>
+
+              <View style={styles.spacer}></View>
+
+              <View>
+                <Text style={styles.userTitle}>{user.job_title}</Text>
+              </View>
+            </View>
+
+            <View style={styles.userAddressBlock}>
+              <Text style={styles.userAddress}>{user.address}</Text>
+            </View>
+          </TouchableOpacity>
+        );
+      }
+    });
+  }
+};
+
+const renderAdresses = users => {
   if (users && users.data) {
     return users.data.map(user => {
       return (
-        <TouchableOpacity
-          key={user.id}
-          style={styles.userCard}
-          onPress={() => navigation.navigate('Location')}>
-          <Image style={styles.userAvatar} source={{uri: user.avatar}} />
-
-          <View style={styles.infoBlock}>
-            <View style={styles.userName}>
-              <Text>{user.name}</Text>
-            </View>
-
-            <View style={styles.spacer}></View>
-
-            <View style={styles.userTitle}>
-              <Text>{user.job_title}</Text>
-            </View>
-          </View>
-        </TouchableOpacity>
+        <Picker.Item
+          key={user.address}
+          label={`${user.address}`}
+          value={user.address}
+        />
       );
     });
   }
@@ -34,6 +60,7 @@ const renderUsers = (users, {navigation}) => {
 
 export const Users = ({navigation}) => {
   const [users, setUsers] = useState(0);
+  const [address, setAddress] = useState(0);
 
   useEffect(() => {
     fetchData().then(data => setUsers(data));
@@ -45,8 +72,20 @@ export const Users = ({navigation}) => {
 
       <Text style={styles.header}>Users</Text>
 
+      <View style={styles.pickerBlock}>
+        <Text>Address: </Text>
+        <Picker
+          selectedValue={address}
+          style={styles.pickerStyle}
+          onValueChange={(itemValue, itemIndex) => {
+            setAddress(itemValue);
+          }}>
+          {renderAdresses(users)}
+        </Picker>
+      </View>
+
       <ScrollView contentContainerStyle={styles.usersList}>
-        {users ? renderUsers(users, {navigation}) : null}
+        {renderUsers(users, {navigation}, address)}
       </ScrollView>
     </View>
   );
@@ -58,6 +97,18 @@ const styles = StyleSheet.create({
     marginLeft: 20,
     fontWeight: 'bold',
     fontSize: 30,
+  },
+  pickerBlock: {
+    marginLeft: 20,
+
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  pickerStyle: {
+    marginLeft: 20,
+
+    height: 50,
+    width: 100,
   },
   usersList: {
     // height: 800,
@@ -98,6 +149,17 @@ const styles = StyleSheet.create({
     backgroundColor: '#FFF',
     borderWidth: 1,
     borderColor: '#000',
+  },
+  userTitle: {
+    color: 'grey',
+  },
+  userAddressBlock: {
+    position: 'absolute',
+    right: 16,
+    top: 16,
+  },
+  userAddress: {
+    fontSize: 16,
   },
   spacer: {
     marginTop: 5,
